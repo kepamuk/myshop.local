a4f32df7d72e3de53bf7f8fce4f9620ac87c7cb5
Your Name <kepamuk@yandex.ru>
2018-04-12 16:46:07 +0300

    #1.4.3

--- config/config.php
+++ config/config.php
@@ -2,3 +2,20 @@
 
 define('PathPrefix', '../controllers/');
 define('PathPostfix', 'Controller.php');
+
+$template = 'default';
+
+define('TemplatePrefix', "../views/{$template}/");
+define('TemplatePostprefix', '.tpl');
+
+define('TemplateWebPath', "/templates/{$template}/");
+
+require '../library/Smarty/libs/Smarty.class.php';
+$smarty = new Smarty();
+
+$smarty->setTemplateDir(TemplatePrefix);
+$smarty->setCompileDir('../tmp/smarty/templates_c');
+$smarty->setCacheDir('../tmp/smarty/cache');
+$smarty->setConfigDir('../library/Smarty/demo/configs');
+
+$smarty->assign('templateWebPath', TemplateWebPath);
